546e3be6170b5678111846be003ade457f32724f
Reichen <119686482+Breichen1@users.noreply.github.com>
2026-01-19 21:16:56 -0700

    Initial commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,169 @@
+// Copyright (c) 2021-2026 Littleton Robotics
+// http://github.com/Mechanical-Advantage
+//
+// Use of this source code is governed by a BSD
+// license that can be found in the LICENSE file
+// at the root directory of this project.
+
+package frc.robot;
+
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import edu.wpi.first.wpilibj.RobotBase;
+
+/**
+ * This class defines the runtime mode used by AdvantageKit. The mode is always "real" when running
+ * on a roboRIO. Change the value of "simMode" to switch between "sim" (physics sim) and "replay"
+ * (log replay from a file).
+ */
+public final class Constants {
+  public static final Mode simMode = Mode.SIM;
+  public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : simMode;
+
+  public static enum Mode {
+    /** Running on a real robot. */
+    REAL,
+
+    /** Running a physics simulator. */
+    SIM,
+
+    /** Replaying from a log file. */
+    REPLAY
+  }
+
+  public static final class Shooter {
+
+    // CAN IDs
+    public static final int leaderMotorID = 9;
+    public static final int followerMotorID = 10;
+
+    public static final int hoodMotorID = 11;
+
+    public static final int turretMotorID = 12;
+
+    // direction inverts
+    public static final boolean invertLeader = false;
+    public static final boolean invertFollower = false;
+
+    public static final boolean invertHood = false;
+    public static final boolean invertTurret = false;
+
+    // Conversion ratios
+    public static final double shooterGearRatio = 1 / 1;
+    public static final double shooterWheelDiameter = 4;
+
+    public static final double hoodGearRatio = 1 / 1;
+
+    public static final double turretGearRatio = 1 / 1;
+
+    // Closed-loop control constants
+    // Shooter motors
+    public static final double shooterKp = 1.0;
+    public static final double shooterKi = 1.0;
+    public static final double shooterKd = 1.0;
+    public static final double shooterKv = 1.0;
+    public static final double shooterKa = 1.0;
+    public static final double shooterKs = 1.0;
+    public static final double shooterMaxAccel = 1.0;
+    public static final double shooterSpinupTime = 1;
+    public static final double shooterMaxJerk = shooterMaxAccel / shooterSpinupTime;
+    public static final CurrentLimitsConfigs shooterCurrent =
+        new CurrentLimitsConfigs()
+            .withSupplyCurrentLimitEnable(true)
+            .withSupplyCurrentLimit(75)
+            .withSupplyCurrentLowerLimit(40)
+            .withSupplyCurrentLowerTime(0.15);
+
+    // Hood motor
+    public static final double hoodForwardLimit = 1;
+    public static final double hoodReverseLimit = 1;
+    public static final double hoodDeadband = 0.25;
+    public static final double hoodKp = 1.0;
+    public static final double hoodKi = 1.0;
+    public static final double hoodKd = 1.0;
+    public static final CurrentLimitsConfigs hoodCurrent =
+        new CurrentLimitsConfigs()
+            .withSupplyCurrentLimitEnable(true)
+            .withSupplyCurrentLimit(15)
+            .withSupplyCurrentLowerLimit(10)
+            .withSupplyCurrentLowerTime(0.15);
+
+    // Turret motor
+    public static final double turretForwardLimit = 1;
+    public static final double turretReverseLimit = -1;
+    public static final double turretDeadband = 0.5;
+    public static final double turretKp = 1.0;
+    public static final double turretKi = 1.0;
+    public static final double turretKd = 1.0;
+    public static final double turretMaxVel = 1.0;
+    public static final double turretAccelTime = 1;
+    public static final double turretMaxAccel = turretMaxVel / turretAccelTime;
+    public static final CurrentLimitsConfigs turretCurrent =
+        new CurrentLimitsConfigs()
+            .withSupplyCurrentLimitEnable(true)
+            .withSupplyCurrentLimit(50)
+            .withSupplyCurrentLowerLimit(40)
+            .withSupplyCurrentLowerTime(0.15);
+  }
+
+  public static final class intake {
+
+    // CAN IDs
+    public static final int strokerID = 13;
+    public static final int intakeID = 14;
+
+    // direction inverts
+    public static final boolean invertStroker = false;
+    public static final boolean invertIntake = false;
+
+    // Conversion ratios
+    public static final double strokerGearRatio = 1;
+    public static final double strokerGearDiameter = 1;
+
+    // Closed-loop control constants
+    // Stroker motor
+    public static final double strokerForwardLimit = 1;
+    public static final double strokerReverseLimit = 1;
+    public static final double strokerDeadband = 0.25;
+    public static final double strokerKp = 1.0;
+    public static final double strokerKi = 1.0;
+    public static final double strokerKd = 1.0;
+    public static final double strokerMaxVel = 1.0;
+    public static final double strokerAccelTime = 1;
+    public static final double strokerMaxAccel = strokerMaxVel / strokerAccelTime;
+    public static final CurrentLimitsConfigs strokerCurrent =
+        new CurrentLimitsConfigs()
+            .withSupplyCurrentLimitEnable(true)
+            .withSupplyCurrentLimit(30)
+            .withSupplyCurrentLowerLimit(20)
+            .withSupplyCurrentLowerTime(0.15);
+  }
+
+  public static final class feeder {
+
+    // CAN IDs
+    public static final int indexerID = 15;
+    public static final int kickerID = 16;
+
+    // direction inverts
+    public static final boolean invertIndexer = false;
+    public static final boolean invertKicker = false;
+
+    public static final CurrentLimitsConfigs feederCurrent =
+        new CurrentLimitsConfigs()
+            .withSupplyCurrentLimitEnable(true)
+            .withSupplyCurrentLimit(50)
+            .withSupplyCurrentLowerLimit(40)
+            .withSupplyCurrentLowerTime(0.15);
+  }
+
+  public static final class ShooterMaps {
+    // TODO: These MUST be calibrated
+    public static final double[][] defaultMap = {
+      // distance, hood angle, linear shooter velocity
+      {0.97, 0.0, 15.5},
+      {1.02, 10.0, 16.0},
+      {1.25, 15.0, 16.5},
+      {1.45, 20.0, 16.5}
+    };
+  }
+}
---
+++ src/main/java/frc/robot/HardwareConfigs.java
@@ -0,0 +1,83 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+/** Add your docs here. */
+public class HardwareConfigs {
+
+  // TalonFX Configurations for hardware devices
+  public TalonFXConfiguration shooterConfig = new TalonFXConfiguration();
+  public TalonFXConfiguration hoodConfig = new TalonFXConfiguration();
+  public TalonFXConfiguration turretConfig = new TalonFXConfiguration();
+
+  public TalonFXConfiguration strokerConfig = new TalonFXConfiguration();
+  public TalonFXConfiguration intakeConfig = new TalonFXConfiguration();
+
+  public TalonFXConfiguration feederConfig = new TalonFXConfiguration();
+
+  public HardwareConfigs() {
+
+    // Shooter configuration
+    // Mechanical configs
+    shooterConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+    shooterConfig.Feedback.SensorToMechanismRatio = Constants.Shooter.shooterGearRatio;
+    shooterConfig.CurrentLimits = Constants.Shooter.shooterCurrent;
+
+    // closed loop configs
+    shooterConfig.Slot0.kP = Constants.Shooter.shooterKp;
+    shooterConfig.Slot0.kI = Constants.Shooter.shooterKi;
+    shooterConfig.Slot0.kD = Constants.Shooter.shooterKd;
+    shooterConfig.Slot0.kV = Constants.Shooter.shooterKv;
+    shooterConfig.Slot0.kA = Constants.Shooter.shooterKa;
+    shooterConfig.Slot0.kS = Constants.Shooter.shooterKs;
+    shooterConfig.MotionMagic.MotionMagicAcceleration = Constants.Shooter.shooterMaxAccel;
+    shooterConfig.MotionMagic.MotionMagicJerk = Constants.Shooter.shooterMaxJerk;
+
+    // Hood configuration
+    // Mechanical configs
+    hoodConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    hoodConfig.Feedback.SensorToMechanismRatio = Constants.Shooter.hoodGearRatio;
+    hoodConfig.CurrentLimits = Constants.Shooter.hoodCurrent;
+
+    // closed loop configs
+    hoodConfig.Slot0.kP = Constants.Shooter.hoodKp;
+    hoodConfig.Slot0.kI = Constants.Shooter.hoodKi;
+    hoodConfig.Slot0.kD = Constants.Shooter.hoodKd;
+
+    // Turret configuration
+    // Mechanical configs
+    turretConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    turretConfig.Feedback.SensorToMechanismRatio = Constants.Shooter.turretGearRatio;
+    turretConfig.CurrentLimits = Constants.Shooter.turretCurrent;
+
+    // closed loop configs
+    turretConfig.Slot0.kP = Constants.Shooter.turretKp;
+    turretConfig.Slot0.kI = Constants.Shooter.turretKi;
+    turretConfig.Slot0.kD = Constants.Shooter.turretKd;
+    turretConfig.MotionMagic.MotionMagicCruiseVelocity = Constants.Shooter.turretMaxVel;
+    turretConfig.MotionMagic.MotionMagicAcceleration = Constants.Shooter.turretMaxAccel;
+
+    // Stroker configuration
+    // Mechanical configs
+    strokerConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    strokerConfig.Feedback.SensorToMechanismRatio = Constants.intake.strokerGearRatio;
+    strokerConfig.CurrentLimits = Constants.intake.strokerCurrent;
+
+    // closed loop configs
+    strokerConfig.Slot0.kP = Constants.intake.strokerKp;
+    strokerConfig.Slot0.kI = Constants.intake.strokerKi;
+    strokerConfig.Slot0.kD = Constants.intake.strokerKd;
+    strokerConfig.MotionMagic.MotionMagicCruiseVelocity = Constants.intake.strokerMaxVel;
+    strokerConfig.MotionMagic.MotionMagicAcceleration = Constants.intake.strokerMaxAccel;
+
+    // feeder configs
+    // Mechanical configs
+    feederConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    feederConfig.CurrentLimits = Constants.feeder.feederCurrent;
+  }
+}
